2d9de0f66a3c489d38d4373c3f4ff9b5b12c5e66
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-01-27 02:11:33 +0700

    Design

--- themes/bwh/templates/node--provider--teaser.tpl.php
+++ themes/bwh/templates/node--provider--teaser.tpl.php
@@ -19,49 +19,6 @@
           
           
           
-          <div style="overflow: hidden;">
-              <div class="logo-info">
-                
-                <?php
-                  if (isset($content['field_p_logo'][0]['#item']['uri'])) {
-                    echo '<div class="logo">' . bwh_misc_getTrackingUrl(theme('image_style', array( 'path' =>  $content['field_p_logo'][0]['#item']['uri'], 'style_name' => 'logo_provider_page', 'alt' => $content['field_p_logo'][0]['#item']['alt'], 'title' => $content['field_p_logo'][0]['#item']['title'], 'attributes' => array('rel' => 'v:photo')))) . '</div>';
-                  }
-                  else {
-                    echo render($title_prefix), '<h2', $title_attributes,'>', $node->field_p_name['und'][0]['value'] /*$content['field_p_name'][0]['#markup']*/, '</h2>', render($title_suffix);
-                  }
-                  
-                ?>
-                
-                <div class="basic-info" rel="v:itemreviewed">
-                  <div typeof="Organization">
-                    <div>
-                      <?php 
-                        if (!$node->p_data['info']['i_web_hide'] && !empty($node->p_data['info']['i_web'])) {
-                          $goto_link_title = (isset($node->p_data['info']['i_web_display']) && $node->p_data['info']['i_web_display']) ? $node->p_data['info']['i_web_display'] : str_replace(array('http://', 'https://'), '', $node->p_data['info']['i_web']);
-                          echo '<span class="title">Website:</span>' . bwh_misc_getTrackingUrl($goto_link_title, NULL, NULL, NULL, NULL, array('key' => 'rel', 'value' => 'v:url'));
-                        }
-                        ?>
-                    </div>
-                  </div>
-                </div>
-
-                
-              </div> <!-- <div class="logo share">-->
-                
-                          
-              <div class="image">
-                <?php
-                  if (isset($content['field_p_image'][0]['#item']['uri'])) {
-                    echo '<div>' . bwh_misc_getTrackingUrl(theme('image_style', array( 'path' =>  $content['field_p_image'][0]['#item']['uri'], 'style_name' => 'image_provider_page', 'alt' =>  $content['field_p_image'][0]['#item']['alt'], 'title' =>  $content['field_p_image'][0]['#item']['title']))), '</div>';
-                  }
-                  
-                  if (!$node->p_data['info']['i_web_hide'] && !empty($node->p_data['info']['i_web'])) {
-                    echo '<div class="site">' , bwh_misc_getTrackingUrl('Visit ' . $node->field_p_name['und'][0]['value']), '</div>';
-                  }
-                ?>  
-                
-              </div>
-             </div>
               
               
               
