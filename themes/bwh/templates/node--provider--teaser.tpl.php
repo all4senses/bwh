058408cd8718febf4dfa8de4a494efe75d5eeb04
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-01-31 21:18:15 +0700

    Homepage Design

--- themes/bwh/templates/node--provider--teaser.tpl.php
+++ themes/bwh/templates/node--provider--teaser.tpl.php
@@ -24,7 +24,7 @@
                   else {
                     $logo = NULL;
                   }
-                  echo '<h2', $h2_class, '>', bwh_misc_getTrackingUrl('<span class="title">' . $title . '</span>' . $logo, NULL, $node->nid), '</h2>';
+                  echo '<h2', $h2_class, '>', bwh_misc_getTrackingUrl('<span class="title">' . $title . '</span>' . $logo . '<div class="site">Visit ' . $node->field_p_name['und'][0]['value'] . '</div>', NULL, $node->nid), '</h2>';
                   //echo '<h2><a href="' . $node_url . '">' . bwh_misc_getTrackingUrl($logo . '<div class="title">' . $title . '</div>', NULL, $node->nid) . '</a></h2>';
                   //dpm($content);
                   //echo '<div class="about">', render($content['body']), l('(Read Full Review)', 'node/' . $node->nid), '</div>'; 
@@ -32,15 +32,15 @@
                   
                   //dpm($node->p_data['provider_options']);
                   echo '<div class="table">';
-                    echo '<div class="mbg">REFUND POLICY<div>', $node->p_data['info']['i_mbg'], '</div></div>';
-                    echo '<div class="space">STORAGE<div>', $node->p_data['s']['sh']['sp'], '</div></div>';
+                    echo '<div class="mbg">MONEY BACK<div>', $node->p_data['info']['i_mbg'], '</div></div>';
+                    echo '<div class="sp">SPACE<div>', $node->p_data['s']['sh']['sp'], '</div></div>';
                     //echo '<div class="domain">DOMAIN NAME<div>', $node->p_data['provider_options']['Services & Performance']['Domains Allowed -text-'], '</div></div>';
-                    echo '<div class="domain">DOMAIN NAME<div>', $node->p_data['provider_options']['Services & Performance']['Domain Name'], '</div></div>';
-                    echo '<div class="traffic">TRAFFIC<div>', $node->p_data['s']['sh']['bw'], '</div></div>';
+                    echo '<div class="domain">DOMAIN NAME<div>', @$node->p_data['provider_options']['Services & Performance']['Domain Name'], '</div></div>';
+                    echo '<div class="bw">BANDWIDTH<div>', $node->p_data['s']['sh']['bw'], '</div></div>';
                   echo '</div>';
                   
                   echo '<div class="right">';
-                    echo '<div class="price"><div>$', $node->p_data['s']['sh']['fees']['mon'], '</div>STARTING PRICE</div>';
+                    echo '<div class="price"><span>$', $node->p_data['s']['sh']['fees']['mon'], '</span>/mo</div>';
                     //echo '<div class="overall">Overall Score: <span class="count" content="' . $node->bwh_rating_overall . '" property="v:rating">' . $node->bwh_rating_overall . '</span> out of 5</div>'; 
                     echo '<div class="rating"><span class="overall">', $node->bwh_rating_overall, '/5</span>', theme('bwh_misc_fivestar_static', array('rating' => $node->bwh_rating_overall*20, 'stars' => 5, 'tag' => 'overall', 'widget' => array('name' => 'stars', 'css' => 'stars.css'))), '</div>';
                     
